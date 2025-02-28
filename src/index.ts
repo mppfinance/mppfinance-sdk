1010acc7b461506348bd42d0ce517d2e84382133
MPPFinance <dev@mppfinance.xyz>
2025-02-28 15:00:13 +0300

    feat: add testnet support

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export type { CardOptions, CardResult, SpendingRules, MPPFinanceConfig, CardEven
 // v6 - Sun Jan 26 09:07:57 MSK 2025
 // v12 - Thu Feb  6 12:23:22 MSK 2025
 // v18 - Mon Feb 17 11:08:52 MSK 2025
+// v24 - Fri Feb 28 15:00:13 MSK 2025
