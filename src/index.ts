cd0f2ed875bf5f6de2efcdc8d6cebbbcac1a5e6f
MPPFinance <dev@mppfinance.xyz>
2025-03-22 14:42:27 +0300

    feat: add GPT-4 tool example

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export type { CardOptions, CardResult, SpendingRules, MPPFinanceConfig, CardEven
 // v18 - Mon Feb 17 11:08:52 MSK 2025
 // v24 - Fri Feb 28 15:00:13 MSK 2025
 // v30 - Tue Mar 11 17:27:39 MSK 2025
+// v36 - Sat Mar 22 14:42:27 MSK 2025
